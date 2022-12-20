17fdfd58bdbcad031fd8cf827faafdeb88a5da17
kuinemuH <humeniukmatias@gmail.com>
2022-12-19 21:55:37 -0300

    clase 1 Javascript

---
+++ Clase10/app.js
@@ -0,0 +1,88 @@
+//esto es un comentario en una sola linea
+
+/*mas de una linea
+de comentario*/
+
+
+
+//declaracion de variables
+
+/*Tipos de datos.
+
+-Numericos 1.... 1000, reales, 2.9 etc -
+
+-Cadena de caracteres (String) / Palabras.
+Hola mundo, Pedro, N, T, etc.
+
+-Valores Logicos (true y false (1 y 0)).
+
+*/
+
+//var numeros = 96 
+
+//var nombres 
+
+//let palabras = 'pedro' //LET cumple la misma funcion que VAR
+
+//const numeroDePi = 3.1416 //Variables Constantes - Que no cambian
+
+//let esNuevo = true
+
+//let numero = 63
+//console.log('el valor inicial de numero es ', numero)
+
+//let nuevoNumero = 987987987987
+
+//console.log('el valor inicial de numero es ', nuevoNumero)
+
+//nuevoNumero = numero
+
+//console.log('este es el valor que tiene la ultima variable ', nuevoNumero) //NUNCA DEJAR EN EL CODIGO!
+
+//Templete string
+
+//console.log(`el valor inicial es ${numero} y el nuevo numero es ${nuevoNumero} y ${nuevoNumero} es igual a ${numero} `) //manera correcta de encadenar datos 
+
+//let guardoNombre = prompt('ingrese su nombre') 
+//console.log(guardoNombre)
+//let guardoEdad = prompt('ingrese su edad') 
+
+//let numero1 = parseInt (prompt('ingrese el primer numero'))
+
+//let numero2 = parseInt (prompt('ingrese el segundo numero'))
+
+//parseInt cambia los valores a enteros para poder sumarse, sino los toma como strig.
+
+//let resultadoMulti = numero1*numero2
+//let resultadoDivi = numero1/numero2
+//let resultadoSuma = numero1 + numero2
+//console.log(resultadoSuma);
+//el PROMPT siempre toma como STRINGS las variables por lo tanto no se puede sumar
+
+//console.log(typeof(numero1))
+
+//estructura condicional simple SE USA SOLO IF.
+//ESTRUCTURA CONDICIONAL COMPUESTA USANDO EL ELSE, ETC.
+
+let notaAprobado=5
+
+let nota = parseInt (prompt ('ingrese la nota del alumnio'))
+
+//if (nota >= notaAprobado) {
+//    console.log('Aprobado')
+//} 
+
+//else{
+  //  console.log('No Aprobaste')
+//}
+
+//OPERADOR TERNARIO (SE USA SOLO CUALDO TENGO IF y ELSE)
+
+nota>=notaAprobado ?  console.log('Aprobo la materia') : console.log('No Aprobaste')
+
+
+
+
+
+
+
